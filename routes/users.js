b7747bb384c3c101973c6de35503df1cc08b180c
Md Miftahul Islam <98267581+itzmiftah@users.noreply.github.com>
2023-12-30 18:58:56 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
  const plm = require("passport-local-mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/pinterest")
+mongoose.connect("mongodb+srv://miftahulislam138:pinterest123@cluster0.cnjupri.mongodb.net/pinterest?retryWrites=true&w=majority")
 
 const userSchema = new mongoose.Schema({
   username: {
