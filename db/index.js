9ed68021da98558d07cd8b93c91d69d40ef0d9a7
Md Miftahul Islam <miftahulislam138@gmail.com>
2024-03-17 21:33:26 +0530

    mongodb

--- db/index.js
+++ db/index.js
@@ -3,9 +3,10 @@ const mongoose = require("mongoose");
 
 
 const connectDB = async () => {
-  
+
     try {
       const connectionInstance =  await mongoose.connect(`${process.env.MONGODB_URI}/pinterest`);
+      // const connectionInstance = await mongoose.connect('mongodb://127.0.0.1:27017/pinterest')
       console.log(`\n MONGODB Connected || DB Host ${connectionInstance.connection.host} `);
   
     } catch (error) {
